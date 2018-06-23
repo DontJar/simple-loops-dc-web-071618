138a5fc81f2f9a961034f83cbbd735bfe8b43571
Jon Schulman <schulman99@gmail.com>
2018-06-23 02:56:50 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -1,7 +1,6 @@
 Phrase = "Welcome to Flatiron School's Web Development Course!"
 
 def loop_iterator(number_of_times)
-#  phrase = "Welcome to Flatiron School's Web Development Course!"
   counter = 0
   loop do
     counter += 1
@@ -12,33 +11,33 @@ def loop_iterator(number_of_times)
   end
 end
 
-# def times_iterator(number_of_times)
-#   number_of_times.times do
-#     puts "Welcome to Flatiron School's Web Development Course!"
-#   end
-# end
-#
-# def while_iterator(number_of_times)
-#   counter = 0
-#   while counter >= 7
-#     puts
-#   # code your solution here using the "while" keyword
-#   # hint: user a counter to tell the while loop when to stop!
-#
-#
-# end
-#
-# def until_iterator(number_of_times)
-#   # code your solution here using the "until" keyword
-#   # hint: use a counter to tell the until loop when to stop!
-#   phrase = "Welcome to Flatiron School's Web Development Course!"
-#
-# end
-#
-# def for_iterator(number_of_times)
-#   # code your solution here using the "for" keyword
-#   # remember that `for` requires a range of numbers. How can we turn our number into a range?
-#
-#   phrase = "Welcome to Flatiron School's Web Development Course!"
-#
-# end
+def times_iterator(number_of_times)
+  number_of_times.times do
+    puts "#{Phrase}"
+  end
+end
+
+def while_iterator(number_of_times)
+  counter = 0
+  while counter >= 7
+    puts
+  # code your solution here using the "while" keyword
+  # hint: user a counter to tell the while loop when to stop!
+
+
+end
+
+def until_iterator(number_of_times)
+  # code your solution here using the "until" keyword
+  # hint: use a counter to tell the until loop when to stop!
+  phrase = "Welcome to Flatiron School's Web Development Course!"
+
+end
+
+def for_iterator(number_of_times)
+  # code your solution here using the "for" keyword
+  # remember that `for` requires a range of numbers. How can we turn our number into a range?
+
+  phrase = "Welcome to Flatiron School's Web Development Course!"
+
+end
